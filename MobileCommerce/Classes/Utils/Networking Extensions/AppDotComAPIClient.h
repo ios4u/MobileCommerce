f81ab6ce012dd45080b651c3ab27f0864d3ec128
edison7500 <edison7500@gmail.com>
2013-12-12 09:11:55 +0800

    update

---
+++ MobileCommerce/Classes/Utils/Networking Extensions/AppDotComAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  AppDotComAPIClient.h
+//  MobileCommerce
+//
+//  Created by 谢家欣 on 13-12-12.
+//  Copyright (c) 2013年 谢家欣. All rights reserved.
+//
+
+//#import "AFhttp"
+
+@interface AppDotComAPIClient : NSObject
+
+@end
